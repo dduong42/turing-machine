5c70fd3cced423b336aaffcf5a2694a81db1acfc
Daniel DUONG <daniel.duong@outlook.com>
2015-08-09 23:23:06 +0200

    Deserialization handled.
    Some comments: I'm not quite satisfied with the error handling, as
    it is not the same everywhere in the code. parseTMachine returns
    a Maybe TuringMachine but stringToAction uses `error`. Also in the
    Turing module I use (!) to access the hashmap. Maybe I need to
    refactor some things, but I'm kind of lazy.

--- Deserializer.hs
+++ Deserializer.hs
@@ -1,14 +1,20 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards #-}
 
+module Deserializer
+( parseTMachine
+) where
+
 import qualified Data.ByteString.Lazy as BS
 import Data.HashMap.Lazy ( HashMap(..)
+                         , fromList
+                         , toList
                          )
 import Data.Aeson
 import qualified Turing as T
 
 data TransitionInput = TransitionInput { action ::String
-                                       , read :: String
+                                       , read' :: String
                                        , toState :: String
                                        , write :: String
                                        } deriving (Show)
@@ -25,7 +31,7 @@ data MachineInput = MachineInput { name :: String
 instance FromJSON TransitionInput where
     parseJSON = withObject "TransitionInput" $ \o -> do
         { action <- o .: "action"
-        ; read <- o .: "read"
+        ; read' <- o .: "read"
         ; toState <- o .: "to_state"
         ; write <- o .: "write"
         ; return TransitionInput{..}
@@ -43,3 +49,33 @@ instance FromJSON MachineInput where
         ; transitions <- parseJSON transitions'
         ; return MachineInput{..}
         }
+
+-- Turn a string into an Action
+stringToAction :: String -> T.Action
+stringToAction "LEFT" = T.MoveLeft
+stringToAction "RIGHT" = T.MoveRight
+stringToAction _ = error "Unknown action"
+
+-- Turn a TransitionInput into a TransitionAction
+inputToAction :: TransitionInput -> T.TransitionAction
+inputToAction (TransitionInput taction tread ttoState twrite)
+    = T.TransitionAction ttoState (head twrite) (stringToAction taction)
+
+inputToTransitions1 :: String -> TransitionInput -> ((String, Char), T.TransitionAction)
+inputToTransitions1 s input = ((s, (head . read') input), inputToAction input)
+
+inputToTransitions2 :: (String, [TransitionInput]) -> [((String, Char), T.TransitionAction)]
+inputToTransitions2 (s, xs) = fmap (inputToTransitions1 s) xs
+
+inputToTransitions :: HashMap String [TransitionInput] -> HashMap (String, Char) T.TransitionAction
+inputToTransitions = fromList . concat . (fmap inputToTransitions2) . toList
+
+-- Return a TuringMachine for a MachineInput
+inputToTMachine :: MachineInput -> T.TuringMachine
+inputToTMachine (MachineInput mname malpha mblank mstates minit mfinals mtransitions)
+    = T.TuringMachine mname (concat malpha) (head mblank) mstates minit
+                      mfinals (inputToTransitions mtransitions)
+
+-- Parse a Turing Machine from a ByteString
+parseTMachine :: BS.ByteString -> Maybe T.TuringMachine
+parseTMachine = (fmap inputToTMachine) . decode
